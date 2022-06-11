a45034d737672787adfce70d45f61226d970ba90
munna505 <oceanbluemunna@gmail.com>
2022-06-11 16:23:38 +0600

    query is user inputable

--- final.py
+++ final.py
@@ -1,15 +1,9 @@
 import openai
 
 #use your own OpenAI API key
-openai.api_key = 'YOU_API_KEY'
+# openai.api_key = 'YOU_API_KEY'
 
 
-#add query to the original string
-def process_keyword(mykeyword):
-    # use query of your choice
-    query = ''
-    final_query = mykeyword +  query 
-    return str(final_query)
 
 #OpenAI completes your keywords
 def grab_completed_text(stext):
@@ -38,14 +32,28 @@ original_keyword =  original_keyword_untrimmed[1]
 introduction = 'Introduction about ' + original_keyword
 conclusion = 'Conclusion about ' + original_keyword
 chosen_keywords.pop(0)
-chosen_keywords.insert(0, introduction)
-chosen_keywords.append(conclusion)
+
+introduction_query = input("Input your query for the introduction. For example, write an article introduction about bicycles.\n::- ")
+processed_keyword =  introduction_query + original_keyword
+openai_completed_introduction =  grab_completed_text(processed_keyword)
+file_open = open("openai-completed-description.txt", 'a')
+file_open.write("<b>Introduction</b>\n\n")
+file_open.write(f'{openai_completed_introduction}\n\n')
+file_open.write("\n\n")
 
 for keyword in chosen_keywords: 
-    processed_keyword = process_keyword(keyword)
-    completed_text = grab_completed_text(processed_keyword)
+    completed_text = grab_completed_text(keyword)
     file_open = open("openai-completed-description.txt", 'a')
-    file_open.write(f'<b>{processed_keyword}</b>\n\n')
+    file_open.write(f'<b>{keyword}</b>\n')
 
-    file_open.write(completed_text)
+    file_open.write(f'{completed_text}\n\n')
     file_open.write("\n\n")
+
+
+conclusion_query = input("Input your query for the introduction. For example, write a conclusion  about bicycles.\n::- ")
+processed_keyword =  conclusion_query + original_keyword
+openai_completed_introduction =  grab_completed_text(processed_keyword)
+file_open = open("openai-completed-description.txt", 'a')
+file_open.write("<b>Conclusion</b>\n\n")
+file_open.write(f'{openai_completed_introduction}\n\n')
+file_open.write("\n\n")
