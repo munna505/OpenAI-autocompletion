21f993a9f8d7d49d4a8ba1db8d1a6660d57f126f
munna505 <oceanbluemunna@gmail.com>
2022-06-11 16:28:39 +0600

    added comments

--- final.py
+++ final.py
@@ -21,10 +21,11 @@ def grab_completed_text(stext):
 
 
 
-
+# stores queries here
 chosen_keywords = []
 
 
+# opens the people-also-ask.txt file to read the quesitons
 file_open_paan = open('people-also-ask.txt', 'r')
 chosen_keywords = file_open_paan.read().splitlines()
 original_keyword_untrimmed = chosen_keywords[0].split(' ')
@@ -33,6 +34,7 @@ introduction = 'Introduction about ' + original_keyword
 conclusion = 'Conclusion about ' + original_keyword
 chosen_keywords.pop(0)
 
+# completes the introduction query
 introduction_query = input("Input your query for the introduction. For example, write an article introduction about bicycles.\n::- ")
 processed_keyword =  introduction_query + original_keyword
 openai_completed_introduction =  grab_completed_text(processed_keyword)
@@ -41,15 +43,15 @@ file_open.write("<b>Introduction</b>\n\n")
 file_open.write(f'{openai_completed_introduction}\n\n')
 file_open.write("\n\n")
 
+# completes people-also-ask queries
 for keyword in chosen_keywords: 
     completed_text = grab_completed_text(keyword)
     file_open = open("openai-completed-description.txt", 'a')
     file_open.write(f'<b>{keyword}</b>\n')
-
     file_open.write(f'{completed_text}\n\n')
     file_open.write("\n\n")
 
-
+# completes the conclusion query
 conclusion_query = input("Input your query for the introduction. For example, write a conclusion  about bicycles.\n::- ")
 processed_keyword =  conclusion_query + original_keyword
 openai_completed_introduction =  grab_completed_text(processed_keyword)
